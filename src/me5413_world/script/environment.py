c6f2d2513410fa03516e51fdaca698b7a4acd056
Bob <lamchunpok@gmail.com>
2025-05-07 10:02:11 +0000

    new reset method

--- src/me5413_world/script/environment.py
+++ src/me5413_world/script/environment.py
@@ -1,31 +1,25 @@
 #!/usr/bin/env python3
 import math
-import os
-import random
-import subprocess
 import time
-from os import path
-
 import numpy as np
 import rospy
-import sensor_msgs.point_cloud2 as pc2
 from gazebo_msgs.msg import ModelState
-from geometry_msgs.msg import Twist, Pose, PoseWithCovarianceStamped
-from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist, PoseWithCovarianceStamped
 from sensor_msgs.msg import LaserScan
 from squaternion import Quaternion
 from std_srvs.srv import Empty
-from visualization_msgs.msg import Marker
 from visualization_msgs.msg import MarkerArray
-from gazebo_msgs.srv import SetModelState
 import tf
+from visualization_msgs.msg import Marker
 
 GOAL_REACHED_DIST = 0.3
-COLLISION_DIST = 0.4
+COLLISION_DIST = 0.3
 TIME_DELTA = 0.1
 
-odom_topic="/odometry/filtered"
+amcl_topic="/amcl_pose"
+
 imu_topic="/imu/data"
+
 
 
 
@@ -34,8 +28,8 @@ class GazeboEnv:
 
     def __init__(self, environment_dim,ros_package=None,launch_file=None):
         self.environment_dim = environment_dim
-        self.odom_x = 0
-        self.odom_y = 0
+        self.pose_x = 0
+        self.pose_y = 0
 
         self.goal_x = -99
         self.goal_y = -99
@@ -44,7 +38,7 @@ class GazeboEnv:
         self.start_pos_y=0
 
         self.laser_data = np.ones(self.environment_dim) * 10
-        self.last_odom = None
+        self.lat_pose = None
 
         self.gaps = [[-np.pi / 2 - 0.03, -np.pi / 2 + np.pi / self.environment_dim]]
         for m in range(self.environment_dim - 1):
@@ -70,10 +64,13 @@ class GazeboEnv:
         self.laser_sub = rospy.Subscriber(
             "front/scan", LaserScan, self.laser_callback, queue_size=1
         )
-        self.odom = rospy.Subscriber(
-            odom_topic, Odometry, self.odom_callback, queue_size=1
+        self.amcl = rospy.Subscriber(
+            amcl_topic, PoseWithCovarianceStamped, self.amcl_callback, queue_size=1
         )
-        self.reset_odom_pub=rospy.Publisher('/set_pose', PoseWithCovarianceStamped, queue_size=1)
+        self.reset_pose_pub = rospy.Publisher('/initialpose', PoseWithCovarianceStamped, queue_size=10)
+        self.set_pose_pub = rospy.Publisher('/set_pose', PoseWithCovarianceStamped, queue_size=10)
+
+
 
 
     # range as state representation
@@ -99,16 +96,16 @@ class GazeboEnv:
                             break
             angle += scan_msg.angle_increment
 
-    def odom_callback(self, od_data):
-        self.last_odom = od_data
+    def amcl_callback(self, amcl_data):
+        self.last_pose = amcl_data
 
     # Perform an action and read a new state
-    def step(self, action):
-        target = False
+    def step(self, action,prev_distance):
+        target=False
         # Publish the robot action
         vel_cmd = Twist()
         vel_cmd.linear.x = action[0]
-        vel_cmd.angular.z = action[1]
+        vel_cmd.angular.z = action[1]*0.5
         self.vel_pub.publish(vel_cmd)
         #self.publish_markers(action)
 
@@ -137,29 +134,29 @@ class GazeboEnv:
         v_state[:] = self.laser_data[:]
         laser_state = [v_state]
 
-        # Calculate robot heading from odometry data
+        # Calculate robot heading from poseetry data
 
-        self.odom_x = self.last_odom.pose.pose.position.x
-        self.odom_y = self.last_odom.pose.pose.position.y
+        self.pose_x = self.last_pose.pose.pose.position.x
+        self.pose_y = self.last_pose.pose.pose.position.y
 
         quaternion = Quaternion(
-            self.last_odom.pose.pose.orientation.w,
-            self.last_odom.pose.pose.orientation.x,
-            self.last_odom.pose.pose.orientation.y,
-            self.last_odom.pose.pose.orientation.z,
+            self.last_pose.pose.pose.orientation.w,
+            self.last_pose.pose.pose.orientation.x,
+            self.last_pose.pose.pose.orientation.y,
+            self.last_pose.pose.pose.orientation.z,
         )
         euler = quaternion.to_euler(degrees=False)
         angle = round(euler[2], 4)
 
         # Calculate distance to the goal from the robot
         distance = np.linalg.norm(
-            [self.odom_x - self.goal_x, self.odom_y - self.goal_y]
+            [self.pose_x - self.goal_x, self.pose_y - self.goal_y]
         )
-        print("odom",self.odom_x,self.odom_y)
+        # print("pose",self.pose_x,self.pose_y)
 
         # Calculate the relative angle between the robots heading and heading toward the goal
-        skew_x = self.goal_x - self.odom_x
-        skew_y = self.goal_y - self.odom_y
+        skew_x = self.goal_x - self.pose_x
+        skew_y = self.goal_y - self.pose_y
         dot = skew_x * 1 + skew_y * 0
         mag1 = math.sqrt(math.pow(skew_x, 2) + math.pow(skew_y, 2))
         mag2 = math.sqrt(math.pow(1, 2) + math.pow(0, 2))
@@ -181,43 +178,52 @@ class GazeboEnv:
         if distance < GOAL_REACHED_DIST:
             target = True
             done = True
-        # print(self.odom_x,self.odom_y)
-        # print("distance:", distance)
-        #print("angle of robot", math.degrees(theta))
+            # print("goal reached!!!!!!!!!!!!!!!!!")
 
 
         robot_state = [distance, theta, action[0], action[1]]
         state = np.append(laser_state, robot_state)
-        reward = self.get_reward(target, collision, action, min_laser)
+        reward = self.get_reward(target, collision, action, min_laser,distance,prev_distance)
+        if collision:
+            # print("collision detected")
 
-        return state, reward, done, target
+        return state, reward, done, target, collision, distance
 
-    def reset(self):
-        x=np.random.uniform(-0.5,22)
-        if 21<=x<=22:
-            y=np.random.uniform(-22,0.5)
-        else:
-            y=np.random.uniform(-0.8,0.8)
-        angle = np.random.uniform(-np.pi, np.pi)
-        quaternion = tf.transformations.quaternion_from_euler(0.0, 0.0, angle)
-        quaternion_world=tf.transformations.quaternion_from_euler(0.0, 0.0, angle-np.pi/2)
-        # self.respawn(x,y,quaternion_world)
-        self.respawn(x,y,quaternion)
-        # print("jackal spawned at", x,y)
-        self.reset_odom(x,y,quaternion)
-        # set a random goal in empty space in environment
-        self.change_goal(x,y)
-        print("goal set at", self.goal_x,self.goal_y)
-        self.start_pos_x=self.last_odom.pose.pose.position.x if self.last_odom is not None else 0
-        self.start_pos_y=self.last_odom.pose.pose.position.y if self.last_odom is not None else 0
-        # randomly scatter boxes in the environment
-        #self.publish_markers([0.0, 0.0])
+    def reset(self,collision):
+        # x=np.random.uniform(-0.5,22)
+        # if 21<=x<=22:
+        #     y=np.random.uniform(-22,0.5)
+        # else:
+        #     y=np.random.uniform(-0.8,0.8)
+        # angle = np.random.uniform(-np.pi, np.pi)
+        # quaternion = tf.transformations.quaternion_from_euler(0.0, 0.0, angle)
         rospy.wait_for_service("/gazebo/unpause_physics")
         try:
             self.unpause()
         except (rospy.ServiceException) as e:
             print("/gazebo/unpause_physics service call failed")
-
+        if collision:
+            # print("collision resetting")
+            for _ in range(5):
+                vel_cmd = Twist()
+                vel_cmd.linear.x = -1
+                self.vel_pub.publish(vel_cmd)
+                time.sleep(0.1)
+        time.sleep(2)
+        self.respawn()
+        self.change_goal(self.last_pose.pose.pose.position.x,self.last_pose.pose.pose.position.y)
+        # print("goal set at", self.goal_x,self.goal_y)
+        self.publish_goal_marker(self.goal_x, self.goal_y)
+        self.start_pos_x=self.last_pose.pose.pose.position.x if self.last_pose is not None else 0
+        self.start_pos_y=self.last_pose.pose.pose.position.y if self.last_pose is not None else 0
+        quaternion = Quaternion(
+            self.last_pose.pose.pose.orientation.w,
+            self.last_pose.pose.pose.orientation.x,
+            self.last_pose.pose.pose.orientation.y,
+            self.last_pose.pose.pose.orientation.z,
+        )
+        euler = quaternion.to_euler(degrees=False)
+        angle = round(euler[2], 4)
         time.sleep(TIME_DELTA)
 
         rospy.wait_for_service("/gazebo/pause_physics")
@@ -230,11 +236,11 @@ class GazeboEnv:
         laser_state = [v_state]
 
         distance = np.linalg.norm(
-            [self.odom_x - self.goal_x, self.odom_y - self.goal_y]
+            [self.start_pos_x- self.goal_x, self.start_pos_y - self.goal_y]
         )
 
-        skew_x = self.goal_x - self.odom_x
-        skew_y = self.goal_y - self.odom_y
+        skew_x = self.goal_x - self.start_pos_x
+        skew_y = self.goal_y - self.start_pos_y
 
         dot = skew_x * 1 + skew_y * 0
         mag1 = math.sqrt(math.pow(skew_x, 2) + math.pow(skew_y, 2))
@@ -262,30 +268,31 @@ class GazeboEnv:
     def change_goal(self,x,y):
         self.goal_x=-99
         self.goal_y=-99
-        if 21<=y<=22:
-            self.goal_y=y
+        if 21<=x<=22:
+            self.goal_x=x
+            while not -22<=self.goal_y<=0.5:
+                if np.random.rand() < 0.5:
+                    rand = np.random.uniform(-3, -1)
+                else:
+                    rand = np.random.uniform(1, 3)
+                self.goal_y=y+rand
+        else:
             while not 0<=self.goal_x<=22:
                 if np.random.rand() < 0.5:
-                    rand = np.random.uniform(-4, -2)
+                    rand = np.random.uniform(-3, -1)
                 else:
-                    rand = np.random.uniform(2, 4)
+                    rand = np.random.uniform(3, 1)
                 self.goal_x=x+rand
-                # self.goal_x=x+np.random.uniform(2, 4)
-        else:
-            while not 0<=self.goal_y<=22:
-                if np.random.rand() < 0.5:
-                    rand = np.random.uniform(-4, -2)
-                else:
-                    rand = np.random.uniform(2, 4)
-                self.goal_y=y+rand
-                #self.goal_y=y+np.random.uniform(2, 4)
-            self.goal_x=x
+            self.goal_y=y
 
 
-    def reset_odom(self,x,y,quaternion):
+
+
+
+    def reset_pose(self,x,y,quaternion):
         msg = PoseWithCovarianceStamped()
         msg.header.stamp = rospy.Time.now()
-        msg.header.frame_id = "odom"  
+        msg.header.frame_id = "map"  
         msg.pose.pose.position.x = x
         msg.pose.pose.position.y = y
         msg.pose.pose.position.z = 2.6
@@ -294,13 +301,22 @@ class GazeboEnv:
         msg.pose.pose.orientation.y = qy
         msg.pose.pose.orientation.z = qz
         msg.pose.pose.orientation.w = qw
-        self.reset_odom_pub.publish(msg)
+        msg.pose.covariance = [ 0.25, 0.0, 0.0, 0.0, 0.0, 0.0,
+                                0.0, 0.25, 0.0, 0.0, 0.0, 0.0,
+                                0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                                0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                                0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                                0.0, 0.0, 0.0, 0.0, 0.0, 0.06853892326654787]
+        self.reset_pose_pub.publish(msg)
+        # msg.header.frame_id = "odom"
+        # self.set_pose_pub.publish(msg)
 
 
-    def respawn(self,jackal_x,jackal_y,jackal_quaternion):
+
+    def respawn(self,jackal_x=0,jackal_y=0,jackal_quaternion=0):
         rospy.wait_for_service('/gazebo/set_model_state')
         # self.respawn_object("jackal",-jackal_y,jackal_x,self.rotate_z(jackal_quaternion))
-        self.respawn_object("jackal",-jackal_y,jackal_x,jackal_quaternion)
+        # self.respawn_object("jackal",-jackal_y,jackal_x,jackal_quaternion)
         # rospy.sleep(0.1)
         self.respawn_object("person_standing",18,22,[4.999999583255033e-07, -5.000000416744966e-07, -0.7071063120935576, 0.7071072502792263])
         # rospy.sleep(0.1)
@@ -324,7 +340,7 @@ class GazeboEnv:
         model_state.pose.orientation.w = qw
         self.set_state.publish(model_state)
 
-    def rotate_z(self,quaternion,degree=-80):
+    def rotate_z(self,quaternion,degree=90):
 
         radians = math.radians(degree)
         half_angle = radians / 2.0
@@ -334,68 +350,39 @@ class GazeboEnv:
 
         return transformed_quaternion
 
-    # def publish_markers(self, action):
-    #     # Publish visual data in Rviz
-    #     markerArray = MarkerArray()
-    #     marker = Marker()
-    #     marker.header.frame_id = "odom"
-    #     marker.type = marker.CYLINDER
-    #     marker.action = marker.ADD
-    #     marker.scale.x = 0.1
-    #     marker.scale.y = 0.1
-    #     marker.scale.z = 0.01
-    #     marker.color.a = 1.0
-    #     marker.color.r = 0.0
-    #     marker.color.g = 1.0
-    #     marker.color.b = 0.0
-    #     marker.pose.orientation.w = 1.0
-    #     marker.pose.position.x = self.goal_x
-    #     marker.pose.position.y = self.goal_y
-    #     marker.pose.position.z = 0
+    def publish_goal_marker(self, x, y):
+        pub = rospy.Publisher("visualization_marker", Marker, queue_size=10)
+        rospy.sleep(1)  # Give time to establish connection
 
-    #     markerArray.markers.append(marker)
+        marker = Marker()
+        marker.header.frame_id = "map"
+        marker.header.stamp = rospy.Time.now()
+        marker.ns = "dot_marker"
+        marker.id = 0
+        marker.type = Marker.SPHERE
+        marker.action = Marker.ADD
 
-    #     self.publisher.publish(markerArray)
+        marker.pose.position.x = x
+        marker.pose.position.y = y
+        marker.pose.position.z = 0
+        marker.pose.orientation.x = 0.0
+        marker.pose.orientation.y = 0.0
+        marker.pose.orientation.z = 0.0
+        marker.pose.orientation.w = 1.0
 
-    #     markerArray2 = MarkerArray()
-    #     marker2 = Marker()
-    #     marker2.header.frame_id = "odom"
-    #     marker2.type = marker.CUBE
-    #     marker2.action = marker.ADD
-    #     marker2.scale.x = abs(action[0])
-    #     marker2.scale.y = 0.1
-    #     marker2.scale.z = 0.01
-    #     marker2.color.a = 1.0
-    #     marker2.color.r = 1.0
-    #     marker2.color.g = 0.0
-    #     marker2.color.b = 0.0
-    #     marker2.pose.orientation.w = 1.0
-    #     marker2.pose.position.x = 5
-    #     marker2.pose.position.y = 0
-    #     marker2.pose.position.z = 0
+        # Dot size (very small sphere)
+        marker.scale.x = 0.5 # diameter in meters
+        marker.scale.y = 0.5
+        marker.scale.z = 0.5
 
-    #     markerArray2.markers.append(marker2)
-    #     self.publisher2.publish(markerArray2)
-
-    #     markerArray3 = MarkerArray()
-    #     marker3 = Marker()
-    #     marker3.header.frame_id = "odom"
-    #     marker3.type = marker.CUBE
-    #     marker3.action = marker.ADD
-    #     marker3.scale.x = abs(action[1])
-    #     marker3.scale.y = 0.1
-    #     marker3.scale.z = 0.01
-    #     marker3.color.a = 1.0
-    #     marker3.color.r = 1.0
-    #     marker3.color.g = 0.0
-    #     marker3.color.b = 0.0
-    #     marker3.pose.orientation.w = 1.0
-    #     marker3.pose.position.x = 5
-    #     marker3.pose.position.y = 0.2
-    #     marker3.pose.position.z = 0
-
-    #     markerArray3.markers.append(marker3)
-    #     self.publisher3.publish(markerArray3)
+        # Color (e.g., blue)
+        marker.color.r = 1.0
+        marker.color.g = 0.0
+        marker.color.b = 0.0
+        marker.color.a = 1.0  # Fully visible
+        marker.lifetime = rospy.Duration(0)  # 0 = forever
+        pub.publish(marker)
+ 
 
     @staticmethod
     def observe_collision(laser_data):
@@ -406,25 +393,26 @@ class GazeboEnv:
         return False, False, min_laser
 
     @staticmethod
-    def get_reward(target, collision, action, min_laser):
+    def get_reward(target, collision, action, min_laser,distance,prev_distance):
         if target:
             return 100.0
         elif collision:
             return -100
+        elif prev_distance is not None:
+            progress = prev_distance - distance
+            return progress * 10.0
         else:
-            r3 = lambda x: 1 - x if x < 1 else 0.0
-            return action[0] / 2 - abs(action[1]) / 2 - r3(min_laser) / 2 
+            return 0
         
 if __name__ == "__main__":
     env = GazeboEnv(20)
     time.sleep(1)
-    for i in range(-0,-180,-5):
-        print("the angle is",i)
-        radians = math.radians(i)
-        quart=tf.transformations.quaternion_from_euler(0.0, 0.0, radians)
-        env.respawn(20, 0.5, quart)
-        env.reset_odom(20,0.5, quart)
-        time.sleep(1)
+    quart=[0,0,0,1]
+    pose=[[1,0],[2,0],[3,0],[4,0],[5,0],[6,0],[7,0],[8,0],[9,0],[10,0],[11,0],[12,0],[13,0],[14,0],[15,0],[16,0],[17,0],[18,0],[19,0]]
+    for x, y in pose:
+        env.respawn(x,y, quart)
+        env.reset_pose(x,y, quart)
+        time.sleep(3)
 
 
 
--- src/me5413_world/script/training.py
+++ src/me5413_world/script/training.py
@@ -18,10 +18,12 @@ def evaluate(network, epoch, eval_episodes=5):
         count = 0
         state = env.reset()
         done = False
+        prev_distance = None
         while not done and count < 501:
             action = network.get_action(np.array(state))
             a_in = [(action[0] + 1) / 2, action[1]]
-            state, reward, done, _ = env.step(a_in)
+            state, reward, done, _ , _ ,distance= env.step(a_in,prev_distance)
+            prev_distance=distance
             avg_reward += reward
             count += 1
             if reward < -90:
@@ -78,6 +80,7 @@ np.random.seed(seed)
 state_dim = environment_dim + robot_dim
 action_dim = 2
 max_action = 1
+collision= False
 
 # Create the network
 network = TD3(state_dim, action_dim, max_action)
@@ -102,6 +105,7 @@ epoch = 1
 
 count_rand_actions = 0
 random_action = []
+prev_distance = None
 
 # Begin the training loop
 while timestep < max_timesteps:
@@ -130,7 +134,7 @@ while timestep < max_timesteps:
             np.save("./results/%s" % (file_name), evaluations)
             epoch += 1
 
-        state = env.reset()
+        state = env.reset(collision)
         done = False
 
         episode_reward = 0
@@ -165,7 +169,8 @@ while timestep < max_timesteps:
 
     # Update action to fall in range [0,1] for linear velocity and [-1,1] for angular velocity
     a_in = [(action[0] + 1) / 2, action[1]]
-    next_state, reward, done, target = env.step(a_in)
+    next_state, reward, done, target,collision,distance = env.step(a_in,prev_distance)
+    prev_distance=distance
     done_bool = 0 if episode_timesteps + 1 == max_ep else int(done)
     done = 1 if episode_timesteps + 1 == max_ep else int(done)
     episode_reward += reward
